051bffdae8655512d9a7bc3347f23c350016636c
JJ Brenner <jbrenner@fh.org>
2017-05-22 16:03:18 -0700

    adding files

---
+++ pairs.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+	var classList = [  'Michael', 'Eli', 'Greg', 'Casey', 'Steven', 'Tim', 
+	'Patrick', 'Esteban', 'Michelle', 'LeeAnna', 'Devren', 
+	'Tyler', 'Frank', 'John', 'James', 'Ryan', 'Rich', 'Sam', 'Andrew', 'Oscar', 'Tarmin'];
+
+
+	// // var saturdayClassList = [
+	// 	'Casey',
+
+	// 	'Catherine',
+	// 	'Christopher',
+	// 	'Danae',
+	// 	'Daniel',
+	// 	'Devren',
+	// 	'Drew',
+	// 	'Eli',
+	// 	'Esteban',
+	// 	'Frank',
+	// 	'George Awad',
+	// 	'Greg',
+	// 	'James',
+	// 	'Jeffrey Hatch',
+	// 	'Jeffrey Schultz',
+	// 	'John',
+	// 	'Jorge Cordova',
+	// 	'Jose',
+	// 	'Kevin',
+	// 	'Kyle',
+	// 	'LeeAnna',
+	// 	'Manish',
+	// 	'Michael',
+	// 	'Michelle',
+	// 	'Nathan',
+	// 	'Nicholas',
+	// 	'Omar',
+	// 	'Oscar',
+	// 	'Patrick',
+	// 	'Paul',
+	// 	'Phillip',
+	// 	'Rich',
+	// 	'Robert',
+	// 	'Ryan',
+		
+	// 	'Saul',
+	// 	'Sherry',
+	// 	'Steven',
+	// 	'Tamy',
+	// 	'Tarmin',
+	// 	'Tim',
+	// 	'Travis',
+	// 	'Tyler',
+	// 	'Wes',
+	// 	'William'
+	// // ];
+	
+	function shuffle (array) {
+ 		var i = 0, j = 0, temp = null
+  		for (i = array.length - 1; i > 0; i -= 1) {
+		    j = Math.floor(Math.random() * (i + 1))
+    		temp = array[i];
+    		array[i] = array[j];
+    		array[j] = temp;
+  		}
+   		return array;
+	}
+
+	function randomPair(array) {
+		var trio = array.splice(0,3);
+		console.log('trio', trio);
+		return trio;
+	}
+	
+
+	$('button').click(function() {
+		var trios = randomPair(shuffle(classList));
+		$('#first').html(trios[0]);
+		$('#second').html(trios[1]);
+		$('#third').html(trios[2]);
+
+		if (classList.length === 0) {
+			// $('.pairs').empty();
+			$('.get-programming').html("That's everyone, get to programming!");
+			$('button').attr('disabled', true);
+
+		}
+	});
+
+
+});
