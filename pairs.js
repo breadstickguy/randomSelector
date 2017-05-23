29b86099030438057371c6ebf907b918b8ccf921
JJ Brenner <jbrenner@fh.org>
2017-05-23 08:11:58 -0700

    adding  https

--- pairs.js
+++ pairs.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
 
 	function getMarvelAPI() {
 		$.ajax({
-			url: 'http://gateway.marvel.com/v1/public/characters?name=Nova&apikey=7ebc44170403dcc0cd0bdf76f2501719',
+			url: 'https://gateway.marvel.com/v1/public/characters?name=Nova&apikey=7ebc44170403dcc0cd0bdf76f2501719',
 			method: 'GET'
 		}).done(function(response) {
 			console.log(response);
