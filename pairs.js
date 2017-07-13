5933599134d988947ad402afa7d17690d19e7d98
JJ Brenner <jbrenner@fh.org>
2017-07-12 17:12:09 -0700

    now recursive and ES6!

--- pairs.js
+++ pairs.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	var classList = [  'Michael', 'Eli', 'Greg', 'Casey', 'Steven', 'Tim', 
+	const classList = [  'Michael', 'Eli', 'Casey', 'Steven', 'Tim', 
 	'Patrick', 'Esteban', 'Michelle', 'LeeAnna', 'Devren', 
-	'Tyler', 'Frank', 'John', 'James', 'Ryan', 'Rich', 'Sam', 'Andrew', 'Oscar', 'Tarmin'];
+	'Tyler', 'Frank', 'John', 'James', 'Ryan', 'Rich', 'Sam', 'Andrew', 'Tarmin'];
 
 
 	// // var saturdayClassList = [
@@ -54,8 +54,8 @@ $(document).ready(function() {
 	// 	'William'
 	// // ];
 	
-	function shuffle (array) {
- 		var i = 0, j = 0, temp = null
+	const shuffle = array => {
+ 		let i = 0, j = 0, temp = null
   		for (i = array.length - 1; i > 0; i -= 1) {
 		    j = Math.floor(Math.random() * (i + 1))
     		temp = array[i];
@@ -65,36 +65,36 @@ $(document).ready(function() {
    		return array;
 	}
 
-	function randomPair(array) {
-		var trio = array.splice(0,3);
-		console.log('trio', trio);
-		return trio;
-	}
+	const randomPair = array => console.log('Best buddy teammates =>', array.splice(0,2));
 
 
-	function getMarvelAPI() {
-		$.get('https://gateway.marvel.com/v1/public/characters?name=Nova&apikey=7ebc44170403dcc0cd0bdf76f2501719')
-			.done(function(response) {
-			console.log(response);
-			}
-		);
-	}
+const recursivePairFinder = (classList) => {
+	// console.log('first =>', classList);
+	if(classList.length <= 0) {
+		return;
+ 	}		
 	
-	getMarvelAPI();
-
-	$('button').click(function() {
-		var trios = randomPair(shuffle(classList));
-		$('#first').html(trios[0]);
-		$('#second').html(trios[1]);
-		$('#third').html(trios[2]);
-
-		if (classList.length === 0) {
-			// $('.pairs').empty();
-			$('.get-programming').html("That's everyone, get to programming!");
-			$('button').attr('disabled', true);
-
-		}
-	});
+	randomPair(shuffle(classList));
+ 	recursivePairFinder(classList);
+}
 
 
-});
+recursivePairFinder(classList);
+
+
+	// $('button').click(function() {
+	// 	var pairs = randomPair(shuffle(classList));
+	// 	$('#first').html(pairs[0]);
+	// 	$('#second').html(pairs[1]);
+	// 	$('#third').html(pairs[2]);
+
+	// 	if (classList.length === 0) {
+	// 		// $('.pairs').empty();
+	// 		$('.get-programming').html("That's everyone, get to programming!");
+	// 		$('button').attr('disabled', true);
+
+	// 	}
+	// });
+
+
+// });
